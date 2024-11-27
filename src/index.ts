a68ec15ae040171397d9bd8c11fdcdb2c65c412e
David Kerber <dave@kerber.io>
2024-11-27 13:27:18 -0600

    Updating exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+export { findContextKeys } from './context_keys/findContextKeys.js';
 export { type EvaluationResult } from './evaluate.js';
+export { allowedContextKeysForRequest } from './simulation_engine/contextKeys.js';
 export { type Simulation } from './simulation_engine/simulation.js';
 export { runSimulation } from './simulation_engine/simulationEngine.js';
 export { type SimulationOptions } from './simulation_engine/simulationOptions.js';
 export { runUnsafeSimulation } from './simulation_engine/unsafeSimulationEngine.js';
-
+export { isWildcardOnlyAction } from './util.js';
 
