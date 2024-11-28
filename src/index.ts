89a7841b97477ff4dd32e4c4f3f7e194f5ab815d
David Kerber <dave@kerber.io>
2024-11-27 20:36:13 -0600

    Adding new exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { BaseConditionKeyType, isConditionKeyArray, type ConditionKeyType } from './ConditionKeys.js';
 export { findContextKeys } from './context_keys/findContextKeys.js';
 export { type EvaluationResult } from './evaluate.js';
 export { allowedContextKeysForRequest } from './simulation_engine/contextKeys.js';
@@ -5,5 +6,5 @@ export { type Simulation } from './simulation_engine/simulation.js';
 export { runSimulation } from './simulation_engine/simulationEngine.js';
 export { type SimulationOptions } from './simulation_engine/simulationOptions.js';
 export { runUnsafeSimulation } from './simulation_engine/unsafeSimulationEngine.js';
-export { isWildcardOnlyAction } from './util.js';
+export { isWildcardOnlyAction, typeForContextKey } from './util.js';
 
