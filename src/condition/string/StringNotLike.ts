7228078896a3fbba926df9c7b10278bb8b3b5c63
David Kerber <dave@kerber.io>
2024-12-14 13:59:30 -0600

    StringNotLike explains

--- src/condition/string/StringNotLike.ts
+++ src/condition/string/StringNotLike.ts
@@ -1,11 +1,37 @@
+import { ConditionValueExplain } from "../../explain/statementExplain.js";
+import { convertIamString } from "../../util.js";
 import { BaseConditionOperator } from "../BaseConditionOperator.js";
-import { StringLike } from "./StringLike.js";
+import { resolvedValue } from "../conditionUtil.js";
 
 export const StringNotLike: BaseConditionOperator = {
   name: 'StringNotLike',
   matches: (request, keyValue, policyValues) => {
-    return !StringLike.matches(request, keyValue, policyValues)
-  },
+      const explains: ConditionValueExplain[] = policyValues.map((value) => {
+        const {pattern, errors} = convertIamString(value, request, {replaceWildcards: true})
+        if(errors && errors.length > 0) {
+          return {
+            value,
+            matches: false,
+            errors
+          }
+        }
+
+        const matches = !pattern.test(keyValue)
+        return {
+          value,
+          matches,
+          resolvedValue: resolvedValue(value, request),
+        }
+      })
+
+
+      const overallMatch = explains.some(explain => explain.matches)
+      return {
+        matches: overallMatch,
+        explains
+      }
+
+    },
   allowsVariables: true,
   allowsWildcards: true
 }
