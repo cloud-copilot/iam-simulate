66cddaa2a1910d9ac01d5d23a21014062809a442
David Kerber <dave@kerber.io>
2024-12-17 22:18:35 -0600

    Adding docs

--- src/explain/statementExplain.ts
+++ src/explain/statementExplain.ts
@@ -30,27 +30,63 @@ export interface ConditionValueExplain {
 }
 
 export interface ConditionExplain {
+
+  /**
+   * The operation that was used in the condition statement
+   */
   operator: string;
+
+  /**
+   * The value being matched in the condition statement
+   */
   conditionKeyValue: string
+
+  /**
+   * The resolved vaue of the condition key
+   */
   resolvedConditionKeyValue?: string
   values: ConditionValueExplain | ConditionValueExplain[]
+
+  /**
+   * The values from the request context that were not matched by a condition with a set operation
+   */
   unmatchedValues?: string[]
+
+  /**
+   * Whether the condition matches the request
+   */
   matches: boolean
+
+  /**
+   *
+   */
   matchedBecauseMissing?: boolean
+
+
+  /**
+   * Failed because the context key was missing from the request.
+   */
   failedBecauseMissing?: boolean
+
+  /**
+   * Failed because the context key was required to be a string but was was
+   * an instead an array. Caused by a set operation being used on a single value
+   * context key.
+   */
   failedBecauseArray?: boolean
+
+  /**
+   * Failed because the context value was required to be an array but was not.
+   * Caused by a set operation being used on a context key that was not an array.
+   */
   failedBecauseNotArray?: boolean
+  /**
+   * Was the base operator in the condition statment not found
+   */
   missingOperator?: boolean
 }
 
 export interface StatementExplain {
-  // request: {
-  //   action: string
-  //   principal: string
-  //   resource: string
-  //   context: Record<string, string | string[]>
-  // }
-
   actionMatch: boolean
   resourceMatch: boolean
   principalMatch: ExplainPrincipalMatch
