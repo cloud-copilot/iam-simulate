be26ed88e51da293e44e2dac721531bbdae6a0b8
David Kerber <dave@kerber.io>
2025-05-26 14:42:42 -0500

    feat: sts:GetCallerIdentity is always allowed

--- src/services/StsServiceAuthorizer.ts
+++ src/services/StsServiceAuthorizer.ts
@@ -1,11 +1,22 @@
-import { ResourceAnalysis } from '../evaluate.js'
+import { RequestAnalysis, ResourceAnalysis } from '../evaluate.js'
 import { RequestResource } from '../request/requestResource.js'
 import { DefaultServiceAuthorizer } from './DefaultServiceAuthorizer.js'
+import { ServiceAuthorizationRequest } from './ServiceAuthorizer.js'
 
 /**
  * The default authorizer for services.
  */
 export class StsServiceAuthorizer extends DefaultServiceAuthorizer {
+  public authorize(request: ServiceAuthorizationRequest): RequestAnalysis {
+    if (request.request.action.value().toLowerCase() === 'sts:getcalleridentity') {
+      return {
+        result: 'Allowed',
+        sameAccount: true
+      }
+    }
+    return super.authorize(request)
+  }
+
   /**
    * Determines if the service trusts the principal's Account's IAM policies
    *
@@ -18,9 +29,16 @@ export class StsServiceAuthorizer extends DefaultServiceAuthorizer {
     resourceAnalysis: ResourceAnalysis,
     resource: RequestResource
   ): boolean {
+    //If there is no resource policy, the service trusts the principal's account IAM policies
     if (sameAccount && resourceAnalysis.result === 'NotApplicable') {
       return true
     }
+
+    /*
+      If there is a resource policy, for instance a role trust policy,
+      the trust policy must explicitly allow the principal's account,
+      even if the principal and resource are in the same account.
+    */
     return resourceAnalysis.allowStatements.some(
       (statement) => statement.principalMatch === 'AccountLevelMatch'
     )
